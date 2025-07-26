c400174da4d96ab89659f759c0aa811303ae4161
Arkasha-P <severe90000@gmail.com>
2025-07-26 19:18:12 +0300

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+systemctl stop cpu_temp_monitor
+systemctl daemon-reload
+rm /etc/systemd/system/cpu_temp_monitor.service
+rm /usr/local/bin/cpu_temp_monitor.sh
+rm /var/log/cpu_temp_monitor.log
+sed -i '/tempmon/' ~/.bashrc
